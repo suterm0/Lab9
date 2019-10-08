8ee2f35c2f4964fc41ea8157cf7732ce6a43bb4a
Michael Suter <suterm@hartwick.edu>
2019-10-08 08:37:46 -0400

    assignment

---
+++ Exercise30.py
@@ -0,0 +1,27 @@
+# Michael Suter
+# Lab #9
+# 10.08.19
+
+people = 30
+cars = 40
+trucks = 15
+
+
+if cars > people:
+    print("We should take the cars")
+elif cars < people:
+    print("We should not take the cars.")
+else:
+    print("We cant decide.")
+
+if trucks > cars:
+    print("Thats too many trucks.")
+elif trucks < cars:
+    print("Maybe we could still take the trucks")
+else:
+    print("We still can't decide")
+
+if people > trucks:
+    print("Alright, let's just take the trucks")
+else:
+    print("Fine, lets stay home then")
